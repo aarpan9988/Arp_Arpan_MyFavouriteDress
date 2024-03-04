14e09f79547af90072096b5aab2e87665506215e
aarpan9988 <W0820839@myscc.ca>
2024-03-03 22:35:42 -0500

    Assignment 5

--- newApp/Arp_Arpan_MyFavouriteDress/src/app/app.module.ts
+++ newApp/Arp_Arpan_MyFavouriteDress/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypeFilterPipe } from './content-type-filter.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
+import { CreateContentComponent } from './create-content/create-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentTypeFilterPipe,
-    HoverAffectDirective
+    HoverAffectDirective,
+    CreateContentComponent
   ],
   imports: [
     BrowserModule,
